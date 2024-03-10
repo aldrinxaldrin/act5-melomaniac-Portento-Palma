afee340ed792258e5baf8058be804d36bda007ed
aldrinxaldrin <159996113+aldrinxaldrin@users.noreply.github.com>
2024-03-10 13:33:19 +0800

    song7.php added

---
+++ song7.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heaven Knows - Chords and Lyrics</title>
+</head>
+<style>
+        body {
+            font-family: 'verdana', 'Arial';
+            background-color: #727780; 
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        h1 {
+            font-size: 3em;
+            color: #000000;
+            margin-bottom: 30px;
+            margin-top: 500px;
+
+        }
+
+        pre {
+            white-space: pre-wrap;
+            font-family: 'Helvetica', 'Helvetica';
+            font-size: 1em;
+            font-weight: bold;
+            color: #000000;
+            margin: auto;
+        }
+        .back {
+            text-decoration: none;
+            font-family: 'verdana', 'Arial';
+            color: black;
+        }
+    </style>
+<body>
+
+<?php
+echo "<h1>Heaven Knows - Chords and Lyrics</h1>\n";
+echo "<pre>\n";
+
+// Intro
+echo "[AM7] [Am] [E] [B7] [G] [B7]\n";
+echo "[EM7] [Asus2] x3\n";
+echo "[F#m] [B]\n\n";
+
+// Verse 1
+echo "           E              Asus2\n";
+echo "There are times when I’m lying in my bed\n";
+echo "      E                        Asus2\n";
+echo "How I bellow and cry from this stupid game\n";
+echo "       E             Asus2                   E\n";
+echo "And my eyes are like windshields on a rainy day\n";
+echo "                    Asus2\n";
+echo "Almost rubbed down, swelling, as I keep on\n\n";
+
+// Refrain
+echo "G#m                Asus2\n";
+echo "Dipping my face in these cold hands of mine\n";
+echo "G#m                F#m     B    E\n";
+echo "Heaven knows how embittered I am\n\n";
+
+// Chorus
+echo "                      Asus2           G#m\n";
+echo "‘Cause this angel has flown away from me\n";
+echo "              F#m     B    E\n";
+echo "Leaving me in drunken misery\n";
+echo "              Asus2                 G#m\n";
+echo "I should have clipped her wings and made her mine\n";
+echo "    F#m       B\n";
+echo "For all eternity\n";
+echo "                   Asus2           G#m\n";
+echo "Now this angel has flown away from me\n";
+echo "                  F#m         B        E\n";
+echo "Thought I had the strength to set her free\n";
+echo "           Asus2           G#m\n";
+echo "Did what I did because I love her so\n";
+echo "DM7                             FM7\n";
+echo "Will she ever find her way back home to me\n";
+echo "    EM7  Asus2  F#m  B\n";
+echo "Aah…\n\n";
+
+// Verse 2
+echo "       E                  Asus2\n";
+echo "I’m so tired, I feel like catching forty winks\n";
+echo "      E              Asus2\n";
+echo "Being up all night in this elbow room\n";
+echo "       Am              E\n";
+echo "That puts me in a trance\n";
+echo "      Asus2                    E      Asus2\n";
+echo "Where hopes and dreams come true\n";
+echo "       E        Asus2           E        Asus2\n";
+echo "Now my lips are burning and my eyes are hurting\n";
+echo "          G#m                  Asus2\n";
+echo "From this fuse I mixed till I light another\n";
+echo "G#m               Asus2\n";
+echo "Cigarette just to pass my time, oh\n";
+echo "G#m                F#m     B   E\n";
+echo "Heaven knows how embittered I am\n\n";
+
+// Chorus
+echo "                      Asus2           G#m\n";
+echo "‘Cause this angel has flown away from me\n";
+echo "              F#m     B    E\n";
+echo "Leaving me in drunken misery\n";
+echo "              Asus2                 G#m\n";
+echo "I should have clipped her wings and made her mine\n";
+echo "    F#m       B\n";
+echo "For all eternity\n";
+echo "                   Asus2           G#m\n";
+echo "Now this angel has flown away from me\n";
+echo "                  F#m         B        E\n";
+echo "Thought I had the strength to set her free\n";
+echo "           Asus2           G#m\n";
+echo "Did what I did because I love her so\n";
+echo "DM7                             FM7\n";
+echo "Will she ever find her way back home to me\n";
+echo "   EM7  CM7 D Am E\n";
+echo "Aah…\n\n";
+
+echo "</pre>\n";
+?>
+
+<br>
+<div>
+    <a class="back" href="index.php">>>> Go Back <<<</a>
+</div>
+</body>
+</html>
