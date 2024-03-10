8277590e09953dbac85b39e46e213d471cde24c9
aldrinxaldrin <159996113+aldrinxaldrin@users.noreply.github.com>
2024-03-10 13:32:45 +0800

    song4.php added

---
+++ song4.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paninindigan Kita - Chords and Lyrics</title>
+</head>
+<style>
+        body {
+            font-family: 'verdana', 'Arial';
+            background-color: #687981; 
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            height: 100vh;
+        }
+
+        h1 {
+            font-size: 3em;
+            color: #000000;
+            margin-bottom: 30px;
+            margin-top: 100px;
+        }
+
+        pre {
+            white-space: pre-wrap;
+            font-family: 'Helvetica', 'Helvetica';
+            font-size: 1em;
+            font-weight: bold;
+            color: #000000;
+            margin: auto;
+        }
+        .back {
+            text-decoration: none;
+            font-family: 'verdana', 'Arial';
+            color: black;
+        }
+    </style>
+<body>
+
+<?php
+echo "<h1>Paninindigan Kita - Chords and Lyrics</h1>\n";
+echo "<pre>\n";
+
+echo "[Intro]\n\n";
+echo "G D G D\n\n";
+
+echo "[Verse 1]\n";
+echo "    Gmaj7                 Dmaj7\n";
+echo "Kakainis na ayaw mong maniwalang mahal kita\n";
+echo "       Gmaj7                   Dmaj7\n";
+echo "Ano pa bang kailangan kong patunayan sa'yo sinta?\n";
+echo "       Em7                     Dmaj7      Bm7\n";
+echo "Pero naiintindihan ko naman, iniiwasan mo lang masaktan\n";
+echo "    Em7                       A      D     A\n";
+echo "'Di ka madedehado, kung sasagutin mo lang ako\n\n";
+
+echo "[Chorus]\n";
+echo "         D\n";
+echo "Paninindigan kita, oo\n";
+echo "      F#m\n";
+echo "Mamahalin kitang buong-buo\n";
+echo "Em7                         D\n";
+echo "Kahit sa pagtanda, ako'y sa'yo\n";
+echo "         Gmaj7\n";
+echo "Paninindigan kita, oo\n";
+echo "          F#m7\n";
+echo "Anumang sabihin ng magulong mundo\n";
+echo "Em7                        Bm7\n";
+echo "Kahit ayaw nilang ako'y sa'yo\n";
+echo "A              Gmaj7 Dmaj7\n";
+echo "Ika'y iingatan ko\n\n";
+
+echo "[Verse 2]\n";
+echo "      Gmaj7                Dmaj7\n";
+echo "Kakakilig ka sa tuwing lumalapit ka na sinta\n";
+echo "      Gmaj7                  Dmaj7\n";
+echo "Nawawala ang angas ko, pilit man na itago pa\n";
+echo "              Em7\n";
+echo "Mm, grabe kasi kung pa'no mo 'ko itahan\n";
+echo "  D            Bm\n";
+echo "Kapag napupuno na 'ko\n";
+echo "         Em7                        A\n";
+echo "Sa ingay ng paligid, ikaw ang aking katahimikan\n\n";
+
+echo "[Chorus]\n";
+echo "         D\n";
+echo "Paninindigan kita, oo\n";
+echo "      F#m\n";
+echo "Mamahalin kitang buong-buo\n";
+echo "Em7                         D\n";
+echo "Kahit sa pagtanda, ako'y sa'yo\n";
+echo "         Gmaj7\n";
+echo "Paninindigan kita, oo\n";
+echo "          F#m7\n";
+echo "Anumang sabihin ng magulong mundo\n";
+echo "Em7                        Bm7\n";
+echo "Kahit ayaw nilang ako'y sa'yo\n";
+echo "A              Gmaj7 F#m7\n";
+echo "Ika'y iingatan ko\n";
+echo "         Em7                 Bm7 A\n";
+echo "Paninindigan kita, ah-ah, ah-ah\n\n";
+
+echo "[Bridge]\n";
+echo "    Em7                 F#m7\n";
+echo "Sasamahan ka kahit na napapalibutan ng mga problema\n";
+echo "   Em7                   A\n";
+echo "Sa hirap at ginhawa, dadamayan kita, sinta\n";
+echo "         G\n";
+echo "Paninindigan kita, oo\n";
+echo "       F#m7\n";
+echo "Kahit alam kong, tayo'y magbabago\n";
+echo "Em7                       D\n";
+echo "Magmula umpisa hanggang dulo\n";
+echo "         G\n";
+echo "Paninindigan kita\n";
+echo "         F#m7\n";
+echo "Paninindigan kita\n";
+echo "              Em7\n";
+echo "'Wag kang mag-alala\n";
+echo "              A\n";
+echo "Ako'y siguradong-sigurado\n\n";
+
+echo "[Chorus]\n";
+echo "         D\n";
+echo "Paninindigan kita, oo\n";
+echo "      F#m\n";
+echo "Mamahalin kitang buong-buo\n";
+echo "Em7                         D\n";
+echo "Kahit sa pagtanda, ako'y sa'yo\n";
+echo "         Gmaj7\n";
+echo "Paninindigan kita, oo\n";
+echo "          F#m7\n";
+echo "Anumang sabihin ng magulong mundo\n";
+echo "Em7                        Bm7\n";
+echo "Kahit ayaw nilang ako'y sa'yo\n";
+echo "A              Gmaj7\n";
+echo "Ika'y iingatan ko\n\n";
+
+echo "[Outro]\n";
+echo "         F#m7\n";
+echo "Paninindigan kita, ah-ah-ah\n";
+echo "         Em7\n";
+echo "Paninindigan kita\n";
+echo "         Bm7\n";
+echo "Paninindigan kita\n";
+echo "A              Gmaj7\n";
+echo "Ika'y iingatan ko (Paninindigan kita)\n";
+echo "            F#m7\n";
+echo "At aalagaan ko (Paninindigan kita)\n";
+echo "                     Em7\n";
+echo "Kung andito na rin tayo (Paninindigan kita)\n";
+echo "       A                D\n";
+echo "Panindigan na natin 'to\n";
+
+echo "</pre>\n";
+?>
+
+<br>
+<div>
+    <a class="back" href="index.php">>>> Go Back <<<</a>
+</div>
+</body>
+</html>
