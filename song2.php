1f67102ae78c0bd95cde1f4d04fc4f7f6dd193ae
aldrinxaldrin <159996113+aldrinxaldrin@users.noreply.github.com>
2024-03-10 13:32:14 +0800

    song2.php added

---
+++ song2.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tip Toe - Chords and Lyrics</title>
+</head>
+<style>
+        body {
+            font-family: 'verdana', 'Arial';
+            background-color: #D53877; 
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            height: 100vh;
+        }
+
+        h1 {
+            font-size: 3em;
+            color: #000000;
+            margin-bottom: 30px;
+            margin-top: 100px;
+        }
+
+        pre {
+            white-space: pre-wrap;
+            font-family: 'Helvetica', 'Helvetica';
+            font-size: 1em;
+            font-weight: bold;
+            color: #000000;
+            margin: auto;
+        }
+        .back {
+            text-decoration: none;
+            font-family: 'verdana', 'Arial';
+            color: black;
+        }
+    </style>
+<body>
+
+<?php
+echo "<h1>Tip Toe - Chords and Lyrics</h1>\n";
+echo "<pre>\n";
+
+echo "[Intro]\n";
+echo "C#m7 Bmaj7\n";
+echo "C#m7 Bmaj7\n\n";
+
+echo "[Verse 1]\n";
+echo "C#m7\n";
+echo "Take me back to the ground\n";
+echo "                      Bmaj7\n";
+echo "'Cause girl, you look fine as hell\n";
+echo "        F#m7\n";
+echo "I'm not easily distracted\n";
+echo "B7                       C#m7\n";
+echo "But you're acting like a magnet\n";
+echo "Emaj7\n";
+echo "You pull me in\n\n";
+
+echo "[Pre-Chorus]\n";
+echo "        C#m7\n";
+echo "I'll be putting on a suit\n";
+echo " \n";
+echo "Be tip toeing to you\n";
+echo "          D#m7\n";
+echo "If you're down for it\n";
+echo "       C#m7\n";
+echo "We got nothing else to do\n";
+echo " \n";
+echo "And nothing else to lose\n";
+echo "        Bmaj7\n";
+echo "And I'm down for this\n\n";
+
+echo "[Chorus]\n";
+echo "C#m7\n";
+echo "If you're in the mood\n";
+echo " \n";
+echo "We can take it to the moon\n";
+echo "Bmaj7\n";
+echo "   Just like a movie scene\n";
+echo " \n";
+echo "Table for two\n";
+echo "C#m7\n";
+echo "   No need to be fed\n";
+echo " \n";
+echo "I want you instead\n";
+echo "Bmaj7\n";
+echo "If you're in the mood\n";
+echo " \n";
+echo "We can tip toe to the moon\n\n";
+
+echo "[Verse 2]\n";
+echo "C#m7    N.C.\n";
+echo "Hold up, Hmmm\n";
+echo "Emaj7\n";
+echo "Tell my friends to pinch me hard\n";
+echo "         Bmaj7\n";
+echo "Cause it coudn't be real\n";
+echo "F#m7\n";
+echo "Oh, Is this a dream I'm living\n";
+echo "B7\n";
+echo "Lord help me, please\n";
+echo "         Emaj7\n";
+echo "'Cause I can't get over you\n\n";
+
+echo "[Pre-Chorus]\n";
+echo "        C#m7\n";
+echo "I'll be putting on a suit\n";
+echo " \n";
+echo "Be tip toeing to you\n";
+echo "          D#m7\n";
+echo "If you're down for it\n";
+echo "       C#m7\n";
+echo "We got nothing else to do\n";
+echo " \n";
+echo "And nothing else to lose\n";
+echo "        Bmaj7\n";
+echo "And I'm down for this\n";
+echo "(Are you down for this?)\n\n";
+
+echo "[Chorus]\n";
+echo "C#m7\n";
+echo "If you're in the mood\n";
+echo " \n";
+echo "We can take it to the moon\n";
+echo "Bmaj7\n";
+echo "   Just like a movie scene\n";
+echo " \n";
+echo "Table for two\n";
+echo "C#m7\n";
+echo "   No need to be fed\n";
+echo " \n";
+echo "I want you instead\n";
+echo "Bmaj7\n";
+echo "If you're in the mood\n";
+echo " \n";
+echo "We can tip toe to the moon\n\n";
+
+echo "[Chorus]\n";
+echo "C#m7\n";
+echo "If you're in the mood\n";
+echo " \n";
+echo "We can take it to the moon\n";
+echo "Bmaj7\n";
+echo "   Just like a movie scene\n";
+echo " \n";
+echo "Table for two\n";
+echo "C#m7\n";
+echo "   No need to be fed\n";
+echo " \n";
+echo "I want you instead\n";
+echo "Bmaj7\n";
+echo "If you're in the mood\n";
+echo " \n";
+echo "We can tip toe to the moon\n\n";
+
+echo "[Outro]\n";
+echo "C#m7 Bmaj7\n\n";
+echo "C#m7 Bmaj7 C#m7\n";
+
+echo "</pre>\n";
+?>
+<br>
+<div>
+    <a class="back" href="index.php">>>> Go Back <<<</a>
+</div>
+</body>
+</html>
