5d72befc648ee1df6f5230311f867b4651081d10
aldrinxaldrin <159996113+aldrinxaldrin@users.noreply.github.com>
2024-03-10 13:32:55 +0800

    song5.php added

---
+++ song5.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yakap Sa Dilim - Chords and Lyrics</title>
+</head>
+<style>
+        body {
+            font-family: 'verdana', 'Arial';
+            background-color: #AE1F23; 
+            color: #333;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        h1 {
+            font-size: 3em;
+            color: #000000;
+            margin-bottom: 30px;
+            margin-top: 100px;
+
+        }
+
+        pre {
+            white-space: pre-wrap;
+            font-family: 'Helvetica', 'Helvetica';
+            font-size: 1em;
+            font-weight: bold;
+            color: #000000;
+            margin: auto;
+        }
+        .back {
+            text-decoration: none;
+            font-family: 'verdana', 'Arial';
+            color: black;
+        }
+    </style>
+<body>
+
+<?php
+echo "<h1>Yakap Sa Dilim - Chords and Lyrics</h1>\n";
+echo "<pre>\n";
+
+// Intro
+echo "[F] [Dm] [Gm] [Csus] [F] [Dm] [Gm]\n\n";
+
+// Verse 1
+echo "                    F         Dm\n";
+echo "Sandal mo sana ang ulo mo sa unan\n";
+echo "Gm   Csus       F       Dm\n";
+echo "Katawan mo ay aking kukumutan\n";
+echo "Gm      Csus    Am         Dm\n";
+echo "Mga problema'y iyong malilimutan\n";
+echo "       C#M7        Csus\n";
+echo "Habang tayo'y magkayakap sa dilim\n\n";
+
+// Verse 2
+echo "                         F        Dm\n";
+echo "Huwag mong pigilan kung nais mapaluha\n";
+echo "Gm     Csus      F        Dm\n";
+echo "Pakiramdam mo sana'y guminhawa\n";
+echo "Gm           Csus     Am       Dm\n";
+echo "Kung gusto mo ay magsigarilyo muna\n";
+echo "      C#M7          Csus\n";
+echo "Bago tayo magkayakap sa dilim\n\n";
+
+// Chorus
+echo "Bbsus             Bb7\n";
+echo "Heto na'ng pinakahihintay natin\n";
+echo "Bbsus             Bb7\n";
+echo "Heto na tayo magkayakap sa dilim\n";
+echo "Ebm    Ab7          Ebm     Ab7\n";
+echo "O kay sarap ng mga nakaw na sandali\n";
+echo "C#M7              Csus           pause\n";
+echo "Habang tayo'y magkayakap sa dilim\n\n";
+
+// Outro
+echo "                F#            Ebm\n";
+echo "Halika ka na at sumiping na sa kama\n";
+echo "G#m       C#sus                Ebm\n";
+echo "Lasapin natin ang sarap ng pagsasama\n";
+echo "G#m          C#sus  Bbm     Ebm\n";
+echo "Sa 'ting pag  ibig tayo ay umasa\n";
+echo "DM7                C# C# C#\n";
+echo "Habang tayo'y magkayakap        sa dilim\n";
+
+echo "</pre>\n";
+?>
+
+<br>
+<div>
+    <a class="back" href="index.php">>>> Go Back <<<</a>
+</div>
+</body>
+</html>
